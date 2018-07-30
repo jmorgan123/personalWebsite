b5b39372ba5b4c584821e9da4f063c276a4af080
johnm257 <ohnm4692@gmail.com>
2018-07-30 15:32:55 -0400

    first

--- styles/scripts/myscript.js
+++ styles/scripts/myscript.js
@@ -2,4 +2,4 @@ const grandma = document.getElementsByClassName('grandma')[0];
 document.body.style.backgroundColor = "red";
 grandma.onclick = function(){myscript};
 grandma.addEventListener("click",myscript);
-document.getElementById("demo").innerHTML = "Hello World";
+  document.getElementById("demo").innerHTML = "Hello World"
